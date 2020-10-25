7df64de88d91d1841e8a24acfa5690dd9c7fdc0a
Live session user <rvaguscenko@gmail.com>
2020-10-25 18:04:34 +0000

    MajasDarbs_1

---
+++ multiply.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+        long int reizinatajs1,reizinatajs2,rezultats;
+	printf("Ievadiet 400000 \n");
+	scanf("%ld",&reizinatajs1);
+	printf("Ievadiet 100000 \n");
+	scanf("%ld",&reizinatajs2);
+	rezultats=reizinatajs1*reizinatajs2;	
+	printf("400000*100000=%ld ",rezultats,"\n");
+
+}
